0cbbb325e217eacf1fd3dd9412227eecd7470477
Rishav Anand <rishva343@gmail.com>
2022-11-01 00:18:14 +0530

    expansion_panel_list implemented for possible matches and there scores

--- lib/screens/create_match_screen.dart
+++ lib/screens/create_match_screen.dart
@@ -14,7 +14,6 @@ class PossibleMatches extends ConsumerStatefulWidget {
 }
 
 class _PossibleMatchesState extends ConsumerState<PossibleMatches> {
-  // final List<TeamItem> _data = generateItems(4);
   @override
   Widget build(BuildContext context) {
     final data = ref.read(teamProvider.notifier).possibleMatches();
@@ -23,50 +22,8 @@ class _PossibleMatchesState extends ConsumerState<PossibleMatches> {
       body: Container(
           child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: data.length % 2 == 0
-                  ? data.length ~/ 2
-                  : data.length ~/ 2 + 1,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                    shadowColor: index < Constants.colors.length
-                        ? Constants.colors[index]
-                        : Constants.colors[index % Constants.colors.length],
-                    elevation: 6,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Column(children: [
-                          _buildNeonLightText(
-                              context, data[index * 2].keys.first, index * 2),
-                          Text(data[index * 2].values.first.first),
-                          Text(data[index * 2].values.first.last),
-                        ]),
-                        const Text('Vs'),
-                        Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              _buildNeonLightText(
-                                  context,
-                                  data[index * 2 + 1].keys.first,
-                                  index * 2 + 1),
-                              Text(data[index * 2 + 1].values.first.first),
-                              Text(data[index * 2 + 1].values.first.last),
-                            ]),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-
-            // _buildPanel()
-          ],
+        child: SingleChildScrollView(
+          child: _buildPanel(data),
         ),
       )),
     );
@@ -98,30 +55,75 @@ class _PossibleMatchesState extends ConsumerState<PossibleMatches> {
     );
   }
 
-  // Widget _buildPanel() {
-  //   return ExpansionPanelList.radio(
-  //     initialOpenPanelValue: 2,
-  //     children: _data.map<ExpansionPanelRadio>((TeamItem item) {
-  //       return ExpansionPanelRadio(
-  //           value: item.id,
-  //           headerBuilder: (BuildContext context, bool isExpanded) {
-  //             return ListTile(
-  //               title: Text(item.headerValue),
-  //             );
-  //           },
-  //           body: ListTile(
-  //               title: Text(item.expandedValue),
-  //               subtitle:
-  //                   const Text('To delete this panel, tap the trash can icon'),
-  //               trailing: const Icon(Icons.delete),
-  //               onTap: () {
-  //                 setState(() {
-  //                   _data.removeWhere(
-  //                       (TeamItem currentItem) => item == currentItem);
-  //                 });
-  //               }));
-  //     }).toList(),
-  //   );
-  // }
-
+  Widget _buildPanel(data) {
+    return ExpansionPanelList.radio(
+      children: data.map<ExpansionPanelRadio>((TeamItem item) {
+        return ExpansionPanelRadio(
+            // backgroundColor: item.id < Constants.colors.length
+            //     ? Constants.colors[item.id]
+            //     : Constants.colors[item.id % Constants.colors.length],
+            value: item.id,
+            canTapOnHeader: true,
+            headerBuilder: (BuildContext context, bool isExpanded) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(children: [
+                      _buildNeonLightText(context, item.teamAlpha, item.id * 2),
+                      Text(item.playerAlpha1),
+                      Text(item.playerAlpha2),
+                    ]),
+                    const Text('Vs'),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          _buildNeonLightText(
+                              context, item.teamBeta, item.id * 2 + 1),
+                          Text(item.playerBeta1),
+                          Text(item.playerBeta2),
+                        ]),
+                  ],
+                ),
+              );
+            },
+            body: Padding(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        const Text("Alpha_Score"),
+                        SizedBox(
+                          width: 100,
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(7))),
+                          ),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        const Text("Beta_Score"),
+                        SizedBox(
+                          width: 100,
+                          child: TextField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(7))),
+                          ),
+                        )
+                      ],
+                    ),
+                  ]),
+            ));
+      }).toList(),
+    );
+  }
 }
